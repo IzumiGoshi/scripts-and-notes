a3ff7a0767ff023fbfcaf9459eda9a7d8681de03
IzumiGoshi <93848375+IzumiGoshi@users.noreply.github.com>
2022-01-18 16:49:35 -0500

    Update namer.go
    simplified the code to just provide a name so it can be reused in other scripts.

--- namer.go
+++ namer.go
@@ -14,19 +14,6 @@ import (
    this is a small script to generate a unique
    filename by stringing together words from the
    linux words file.
-
-   ex
-   namer ~/Pictures jpg
-   will generate a file like this
-   /home/IzumiGoshi/Pictures/GwynAugerKentonAngers.jpg
-
-   the default is to use 4 words
-   that are at max 6 characters long.
-
-   this can also be set 
-   namer ~/Pictures jpg 3 3
-   /home/IzumiGoshi/Pictures/MayZedPin.jpg
-
 */
 
 //go:embed english.txt
@@ -34,14 +21,6 @@ var DICTIONARY string
 var DICTIONARY_WORDS []string
 var DIC_LEN int
 
-// returns (name, extension)
-func extension(fname string) (string, string) {
-    split := strings.Split(fname, ".")
-    namespl := split[:len(split)-1]
-    name := strings.Join(namespl, ".")
-    ext := split[len(split)-1]
-    return name, ext
-}
 
 func gen_random() *rand.Rand {
     s1 := rand.NewSource(time.Now().UnixNano())
@@ -49,11 +28,13 @@ func gen_random() *rand.Rand {
     return r1
 }
 
+
 func format_word(word string) string {
     word = strings.Replace(word, "'", "", -1)
     word = strings.Title(word)
     return word
 }
+
 
 func generate_name(word_count int, length int, r *rand.Rand) string {
     var words []string
@@ -80,31 +61,21 @@ func main() {
     DICTIONARY_WORDS = strings.Split(DICTIONARY, "\n")
     DIC_LEN = len(DICTIONARY_WORDS)
 
-    if os.Args[1] == "-h" {
-        fmt.Println("namer   /my/dir   file_extension   word_count   max_word_length")
+    if len(os.Args) > 1 && os.Args[1] == "-h" {
+        fmt.Println("run with namer (options word_count, max_word_length, will default to 4 and 6)")
         os.Exit(0)
     }
 
-    DIR := os.Args[1]
-    FILE_EXTENSION := os.Args[2]
+    fmt.Println(os.Args) 
 
     COUNT := 4  // default
     LENGTH := 6 // default
-    if len(os.Args) == 5 {
-        COUNT, _ = strconv.Atoi(os.Args[3])
-        LENGTH, _ = strconv.Atoi(os.Args[4])
+    if len(os.Args) == 3 {
+        COUNT, _ = strconv.Atoi(os.Args[1])
+        LENGTH, _ = strconv.Atoi(os.Args[2])
     }
 
     r := gen_random()
-    gen_name := ""
-    new_file := ""
-
-    for {
-        gen_name = generate_name(COUNT, LENGTH, r)
-        new_file = DIR + "/" + gen_name + "." + FILE_EXTENSION
-        if _, err := os.Stat(new_file); os.IsNotExist(err) {
-            break
-        }
-    }
-    fmt.Println(new_file)
+    gen_name := generate_name(COUNT, LENGTH, r) 
+    fmt.Println(gen_name)
 }
