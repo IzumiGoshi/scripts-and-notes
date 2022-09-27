6b0e018c15cb0826ca3e6beda9155d829474fde8
IzumiGoshi <93848375+IzumiGoshi@users.noreply.github.com>
2022-09-27 05:37:34 -0400

    Update gdscript rotation.gd

--- gdscript rotation.gd
+++ gdscript rotation.gd
@@ -4,6 +4,10 @@ extends Spatial
 onready var X := Spatial.new()
 onready var Y := Spatial.new()
 onready var Z := Spatial.new()
+onready var zup := Vector3()
+onready var zright := Vector3()
+onready var zforward := Vector3()
+
 onready var model := $Model
 
 func _ready() -> void:
@@ -18,24 +22,24 @@ func _ready() -> void:
 	Z.add_child(model)  # !important
 
 func controls(delta):
-	var u : Vector3 = Z.global_transform.basis.y
-	var r : Vector3 = Z.global_transform.basis.x
-	var f : Vector3 = Z.global_transform.basis.z
+	zup = Z.global_transform.basis.y
+	zright = Z.global_transform.basis.x
+	zforward = Z.global_transform.basis.z
 	
 	if Input.is_key_pressed(KEY_A):
-		X.global_rotate(u, delta)
+		X.global_rotate(zup, delta)
 	if Input.is_key_pressed(KEY_D):
-		X.global_rotate(u, -delta)
+		X.global_rotate(zup, -delta)
 		
 	if Input.is_key_pressed(KEY_W):
-		Y.global_rotate(r, delta)
+		Y.global_rotate(zright, delta)
 	if Input.is_key_pressed(KEY_S):
-		Y.global_rotate(r, -delta)
+		Y.global_rotate(zright, -delta)
 		
 	if Input.is_key_pressed(KEY_Z):
-		Y.global_rotate(f, delta)
+		Y.global_rotate(zforward, delta)
 	if Input.is_key_pressed(KEY_C):
-		Y.global_rotate(f, -delta)
+		Y.global_rotate(zforward, -delta)
 	
 
 func _process(delta: float) -> void:
